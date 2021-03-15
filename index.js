eaddcbb6102317b64e7a6431c285d50ada21e0e4
Eric Li <ericli@umass.edu>
2021-03-15 16:40:07 -0400

    line 234 change

--- index.js
+++ index.js
@@ -232,6 +232,7 @@ express()
     }
   })
   .get('/dima', (req, res) => res.render('pages/dima'))
+  .get('/eric', (req, res) => res.render('pages/eric'))
   .get('/jack', (req, res) => res.render('pages/jack'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
